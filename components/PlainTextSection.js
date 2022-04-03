e7d0765077d77c36d623f18af902d83b3b855382
Daniel Schmid <daniel@nachodevelopment.uk>
2022-04-03 15:29:14 +0100

    push

--- components/PlainTextSection.js
+++ components/PlainTextSection.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function PlainTextSection({details}) {
   return (
     <div className='my-16 mx-auto px-8 max-w-2xl relative'>
-      <p className='font-tertiary font-light leading-7 tracking-widest'>
+      <p className='font-tertiary font-light leading-7 tracking-widest '>
         {details.text}
       </p>
     </div>
